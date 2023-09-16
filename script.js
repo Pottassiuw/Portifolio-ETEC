738f5ed9d875d26ce1068f23cc36c000a83ba014
Pottassiuw <pottassiu@proton.me>
2023-09-16 15:37:12 -0300

    Corrigido

--- script.js
+++ script.js
@@ -17,3 +17,41 @@ menuItems.forEach(function(item) {
         toggleMenu(); // Chama a função para fechar o menu
     });
 });
+
+const carousel = document.querySelector('.carousel');
+const slides = document.querySelectorAll('.slide');
+const bullets = document.querySelectorAll('.bullet');
+const intervalTime = 5000; // Tempo em milissegundos entre as transições (5 segundos)
+let currentIndex = 0;
+
+bullets.forEach((bullet, index) => {
+    bullet.addEventListener('click', () => {
+        goToSlide(index);
+    });
+});
+
+function goToSlide(index) {
+    currentIndex = index;
+    const translateX = -index * 100; // 100% de deslocamento para cada slide
+    carousel.style.transform = `translateX(${translateX}%)`;
+
+    // Ative a classe 'active' no bullet atual e remova-a dos outros
+    bullets.forEach((bullet, i) => {
+        if (i === index) {
+            bullet.classList.add('active');
+        } else {
+            bullet.classList.remove('active');
+        }
+    });
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    goToSlide(currentIndex);
+}
+
+// Configurar um intervalo para avançar automaticamente os slides
+setInterval(nextSlide, intervalTime);
+
+// Inicialize o carrossel com o primeiro slide
+goToSlide(currentIndex);
